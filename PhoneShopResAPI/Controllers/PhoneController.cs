bbafebfb89b9604fb92626b8a08d71366ccf1a5f
Khangnekk <contact.khalgfk@gmail.com>
2023-11-08 07:46:45 +0700

    Create PhoneController.cs

---
+++ PhoneShopResAPI/Controllers/PhoneController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneShopResAPI.Models;
+
+namespace PhoneShopResAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhoneController : Controller
+    {
+
+        private static PhoneStoreContext _phoneStoreContext = new PhoneStoreContext();
+
+        private readonly ILogger<PhoneController> _logger;
+
+        public PhoneController(ILogger<PhoneController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetPhone")]
+        public List<Phone> GetPhones()
+        {
+            List<Phone> result = new List<Phone>();
+            result = _phoneStoreContext.Phones.ToList();
+            return result;
+        }
+    }
+}
